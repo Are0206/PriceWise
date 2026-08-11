50094c720c2fc6ea0793e0ef72920823328a7373
vantaflex <dasaduni2006@gmail.com>
2026-08-11 00:09:10 -0500

    Funcionalidad de búsqueda por supermercado

--- core/views.py
+++ core/views.py
@@ -4,16 +4,24 @@ from products.models import Product
 
 def home(request):
     query = request.GET.get('q', '')
+    search_by = request.GET.get('search_by', 'product')
     products = Product.objects.all()
 
-    if query:
-        products = products.filter(name__icontains=query)
+    if search_by == 'supermarket':
+            # Si supermercado es un campo ForeignKey/Relación en Product:
+            products = products.filter(prices__supermarket__name__icontains=query).distinct()
+            
+            # NOTA: Si el campo en tu modelo se llama diferente (ej: 'store' o 'supermarket' de texto directo):
+            # Usa 'supermarket__icontains=query' en su lugar.
+    else:
+            products = products.filter(name__icontains=query)
 
     return render(
         request,
         "core/index.html",
         {
             "products": products,
-            "query": query
+            "query": query,
+            "search_by": search_by,
         }
     )
