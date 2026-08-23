3562455a19ca81e6d069f9f3d3ebd29160d4666d
cortes1443 <juancortesg12@hotmail.com>
2026-08-22 21:46:50 -0500

    filter by category implemented

--- core/views.py
+++ core/views.py
@@ -1,27 +1,34 @@
 from django.http import HttpResponse 
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product, Category
 
 def home(request):
     query = request.GET.get('q', '')
     search_by = request.GET.get('search_by', 'product')
-    products = Product.objects.all()
+    category_filter = request.GET.get('category', '')
 
-    if search_by == 'supermarket':
-            # Si supermercado es un campo ForeignKey/Relación en Product:
-            products = products.filter(prices__supermarket__name__icontains=query).distinct()
-            
-            # NOTA: Si el campo en tu modelo se llama diferente (ej: 'store' o 'supermarket' de texto directo):
-            # Usa 'supermarket__icontains=query' en su lugar.
-    else:
-            products = products.filter(name__icontains=query)
+    products = Product.objects.all()
+    categories = Category.objects.all()
+
+    if category_filter:
+        products = products.filter(category__slug=category_filter)
+
+    # 2. Aplicamos la búsqueda de texto si el usuario escribió algo
+    if query:
+        match search_by:
+            case 'supermarket':
+                products = products.filter(prices__supermarket__name__icontains=query).distinct()
+            case _:
+                products = products.filter(name__icontains=query)
 
     return render(
         request,
         "core/index.html",
         {
             "products": products,
+            "categories": categories,
             "query": query,
             "search_by": search_by,
+            "category_filter": category_filter,
         }
     )
